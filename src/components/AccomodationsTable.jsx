73bda01e22c808b2435aabf215d10ade7ad6f15c
Dianne <daramirez1205@gmail.com>
2025-01-06 20:24:14 -0800

    fetch data for accommodations table

--- src/components/AccomodationsTable.jsx
+++ src/components/AccomodationsTable.jsx
@@ -1,42 +1,86 @@
 import React from "react";
 import Button from "./Button";
+import { useEffect, useState } from "react";
+import Spinner from "./Spinner";
 
 const AccomodationsTable = () => {
+  const [accommodations, setAccommodations] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "http://localhost/lafreza-server/admin/fetch_accommodations.php",
+          {
+            method: "GET",
+            headers: { "Content-Type": "application/json" },
+            credentials: "include",
+          }
+        );
+        const result = await response.json();
+
+        console.log(accommodations);
+
+        if (response.ok) {
+          const sortedData = result.data.sort((a, b) => a.id - b.id);
+          setAccommodations(sortedData);
+          setLoading(false);
+        } else {
+          setErrorMessage(result.message || "Failed to load accommodations");
+          setLoading(false);
+        }
+      } catch (error) {
+        console.log("An error occured: ", error);
+        setErrorMessage("An unexected error occured. Please try again later.");
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <Spinner />;
+  }
+
+  if (errorMessage) {
+    return <p className="text-center mt-10 text-red-500">{errorMessage}</p>;
+  }
+
   return (
     <table className="w-full">
       <thead className="mb-2">
         <tr className="mb-2">
           <th className="w-24">#</th>
-          <th className="w-1/3">Name</th>
-          <th className="w-1/3">Type</th>
-          <th className="w-1/3">Actions</th>
+          <th className="w-3/5">Name</th>
+          <th className="w-1/5">Type</th>
+          <th className="w-1/5">Actions</th>
         </tr>
       </thead>
       <tbody>
-        <tr className=" py-1">
-          <td className="w-14 py-1 px-2 border">1</td>
-          <td className="w-1/3 py-1 px-2 border">DAISY (GROUND FLOOR)</td>
-          <td className="w-1/3 py-1 px-2 border">Room</td>
-          <td className="w-1/3 py-1 px-2">
-            <div className="flex justify-center gap-2 ">
-              <Button
-                buttonName={"Show more"}
-                buttonColor={"bg-yellow-400"}
-                buttonHoverColor={"hover:bg-yellow-300"}
-              />
-              <Button
-                buttonName={"Edit"}
-                buttonColor={"bg-blue-600"}
-                buttonHoverColor={"hover:bg-blue-500"}
-              />
-              <Button
-                buttonName={"Delete"}
-                buttonColor={"bg-red-600"}
-                buttonHoverColor={"hover:bg-red-500"}
-              />
-            </div>
-          </td>
-        </tr>
+        {accommodations.map((accommodation, index) => (
+          <tr key={accommodation.id} className=" py-1">
+            <td className="w-14 py-1 px-2 border">{index + 1}</td>
+            <td className="w-3/5 py-1 px-2 border">
+              {accommodation.accomodation_name}
+            </td>
+            <td className="w-1/5 py-1 px-2 border">
+              {accommodation.accomodation_type}
+            </td>
+            <td className="w-1/5 py-1 px-2">
+              <div className="flex justify-center gap-2 ">
+                <a
+                  href={`/admin/accommodation/${accommodation.id}`}
+                  className="rounded-lg py-2 px-5 text-gray-900 bg-yellow-400 hover:bg-yellow-300"
+                >
+                  Show more
+                </a>
+              </div>
+            </td>
+          </tr>
+        ))}
       </tbody>
     </table>
   );
