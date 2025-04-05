d352671e17043c5348bd010eb2ec5bbaa4b11323
R4ZZ-AURA <russeljohn_medina@aura.edu.ph>
2025-04-05 13:54:27 +0800

    title page name & logo

--- src/layouts/AdminLayout.jsx
+++ src/layouts/AdminLayout.jsx
@@ -3,16 +3,22 @@ import { Outlet } from "react-router-dom";
 import SidebarMenu from "../pages/Admin/SidebarMenu";
 import Navbar from "../components/Navbar";
 import ContentCanvas from "../components/ContentCanvas";
+import { HelmetProvider } from "react-helmet-async";
 
 const AdminLayout = () => {
   return (
-    <div className="flex bg-gray-200 h-screen">
-      <SidebarMenu />
-      <ContentCanvas>
-        <Navbar />
-        <Outlet />
-      </ContentCanvas>
-    </div>
+    <>
+      <HelmetProvider>
+        <title>La Freza Gold | Admin</title>
+      </HelmetProvider>
+      <div className="flex bg-gray-200 h-screen">
+        <SidebarMenu />
+        <ContentCanvas>
+          <Navbar />
+          <Outlet />
+        </ContentCanvas>
+      </div>
+    </>
   );
 };
 
--- src/layouts/GuestLayout.jsx
+++ src/layouts/GuestLayout.jsx
@@ -2,14 +2,20 @@ import React from "react";
 import Navbar from "../components/Guest/Navbar";
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Guest/Footer";
+import { HelmetProvider } from "react-helmet-async";
 
 const GuestLayout = () => {
   return (
-    <div className="bg-gray-100 text-gray-900 min-h-screen">
-      <Navbar />
-      <Outlet />
-      <Footer />
-    </div>
+    <>
+      <HelmetProvider>
+        <title>La Freza Gold | Home</title>
+      </HelmetProvider>
+      <div className="bg-gray-100 text-gray-900 min-h-screen">
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </div>
+    </>
   );
 };
 
