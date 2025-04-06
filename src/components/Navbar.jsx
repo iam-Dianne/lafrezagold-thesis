c6d952247e2eb92c11cd0d0cec0dbffa0bf13197
R4ZZ-AURA <russeljohn_medina@aura.edu.ph>
2025-04-06 13:12:20 +0800

    hi {username}?

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -50,12 +50,16 @@ const Navbar = () => {
     <div className="flex justify-between text-gray-900 h-10">
       <h1 className="text-2xl font-bold">{title}</h1>
       <div className="relatie profile-icon flex flex-col items-center mr-3">
-        <button className="flex mb-4" onClick={toggleDropdown}>
-          <FaUserCircle size={28} className="mr-1" />
-          <RiArrowDropDownLine size={28} />
-        </button>
+        <h className="absolute top-200 right-8 w-40 text-gray-900 px-3 py-4">
+          Hi
+        </h>
 
-        {activeDropdown && (
+        {/* <button className="flex mb-4" onClick={toggleDropdown}>
+          <FaUserCircle size={28} className="mr-1" />
+          <RiArrowDropDownLine size={28} /> 
+        </button> */}
+
+        {/* {activeDropdown && (
           <ul className="absolute top-20 right-8 w-40 bg-gray-900 rounded-lg text-gray-100 px-3 py-4">
             <li>
               <a
@@ -74,7 +78,7 @@ const Navbar = () => {
               </a>
             </li>
           </ul>
-        )}
+        )} */}
       </div>
     </div>
   );
